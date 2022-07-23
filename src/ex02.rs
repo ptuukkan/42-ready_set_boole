ea4367ac2ab086abe8b066a02e1e1f859d0c1783
Pekka Tuukkanen <ptuukkan@student.hive.fi>
2022-07-23 10:17:57 +0300

    ex02

--- src/ex02.rs
+++ src/ex02.rs
@@ -1,3 +1,15 @@
-fn gray_code(n: u32) -> u32 {
+pub fn gray_code(n: u32) -> u32 {
+	n ^ (n >> 1)
+}
 
+#[test]
+fn test_gray_code() {
+    assert_eq!(gray_code(0), 0);
+	assert_eq!(gray_code(1), 1);
+	assert_eq!(gray_code(2), 3);
+	assert_eq!(gray_code(3), 2);
+	assert_eq!(gray_code(4), 6);
+	assert_eq!(gray_code(9), 13);
+	assert_eq!(gray_code(12), 10);
+	assert_eq!(gray_code(53920), 48112);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod ex00;
 mod ex01;
+mod ex02;
 
 fn main() {
     println!("{}", ex00::adder(5, 5));
 	println!("{}", ex01::multiplier(1, 0));
+	println!("{}", ex02::gray_code(8));
 }
