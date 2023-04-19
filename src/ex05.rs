c54162ef96e369c08e6ab7ce8aec1311145799d9
Pekka Tuukkanen <petuukka@gmail.com>
2023-04-19 21:38:58 +0200

    ex05 wip 2

--- src/ex05.rs
+++ src/ex05.rs
@@ -1,4 +1,41 @@
-use crate::helpers::*;
+use crate::{ast::Node, helpers::*};
+
+enum RewriteRule {
+    DoubleNegation,
+}
+
+fn is_negation(node: &Box<Node<char>>) -> Option<&Box<Node<char>>> {
+    match **node {
+        Node::UnaryExpr { ref op, ref child } => match op {
+            UnaryOperator::Negation => Some(child),
+        },
+        _ => None,
+    }
+}
+
+fn rewrite_rule(node: &mut Node<char>) -> &mut Node<char> {
+    match node {
+        Node::Operand(_) => node,
+        Node::UnaryExpr { op, child } => match op {
+            UnaryOperator::Negation => match is_negation(&*child) {
+                Some(grand_child) => {
+                    child = grand_child;
+                    node
+                }
+                None => node,
+            },
+        },
+        Node::BinaryExpr { op, left, right } => node,
+    }
+}
+
+fn joku(tree: &mut Node<char>) -> Node<char> {
+    match rewrite_rule(tree) {
+        Node::Operand(_) => todo!(),
+        Node::UnaryExpr { op, child } => todo!(),
+        Node::BinaryExpr { op, left, right } => todo!(),
+    };
+}
 
 pub fn negation_normal_form(formula: &str) -> String {
     if let Ok(parse_result) = parse_formula(formula) {
