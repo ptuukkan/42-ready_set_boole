752f1f99c648c56972521273bd8706630c188ec5
Pekka Tuukkanen <petuukka@gmail.com>
2024-01-14 21:05:48 +0100

    Ex10 and 11 working

--- src/ex10.rs
+++ src/ex10.rs
@@ -1,9 +1,15 @@
-fn morton(x: u32) -> u32 {
-    println!("{:x}", x);
-    x
-} 
+// https://fgiesen.wordpress.com/2009/12/13/decoding-morton-codes/
+fn spread(mut n: u32) -> u32 {
+    n = (n ^ (n << 8)) & 0x00ff00ff; // n = ---- ---- fedc ba98 ---- ---- 7654 3210
+    n = (n ^ (n << 4)) & 0x0f0f0f0f; // n = ---- fedc ---- ba98 ---- 7654 ---- 3210
+    n = (n ^ (n << 2)) & 0x33333333; // n = --fe --dc --ba --98 --76 --54 --32 --10
+    n = (n ^ (n << 1)) & 0x55555555; // n = -f-e -d-c -b-a -9-8 -7-6 -5-4 -3-2 -1-0
+    n
+}
 
 pub fn map(x: u16, y: u16) -> f64 {
-    morton(x as u32);
-    1.2
+    let m_x = spread(x as u32);
+    let m_y = spread(y as u32) << 1;
+    let morton = f64::from(m_x + m_y);
+    morton / u32::MAX as f64
 }
--- src/ex11.rs
+++ src/ex11.rs
@@ -1,6 +1,26 @@
+// https://fgiesen.wordpress.com/2009/12/13/decoding-morton-codes/
+fn squish(mut n: u32) -> u16 {
+    n &= 0x55555555; //                 n = -f-e -d-c -b-a -9-8 -7-6 -5-4 -3-2 -1-0
+    n = (n ^ (n >> 1)) & 0x33333333; // n = --fe --dc --ba --98 --76 --54 --32 --10
+    n = (n ^ (n >> 2)) & 0x0f0f0f0f; // n = ---- fedc ---- ba98 ---- 7654 ---- 3210
+    n = (n ^ (n >> 4)) & 0x00ff00ff; // n = ---- ---- fedc ba98 ---- ---- 7654 3210
+    n = (n ^ (n >> 8)) & 0x0000ffff; // n = ---- ---- ---- ---- fedc ba98 7654 3210
+    return n as u16;
+}
+
 pub fn reverse_map(n: f64) -> (u16, u16) {
-    let size = 16;
-    let pixels: u64 = 4294967296;
-    println!("{}", pixels);
-    (0, 0)
+    if n < 0.0 || n > 1.0 {
+        println!("Error");
+        return (0, 0);
+    }
+
+    let morton = n * u32::MAX as f64;
+    let x = squish(morton as u32);
+    let y = squish(morton as u32 >> 1);
+    (x, y)
+}
+
+#[test]
+fn test_reverse_map() {
+
 }
