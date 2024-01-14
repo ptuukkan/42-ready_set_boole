6dfc9d318a4ad36ffaed532a8adb94722cfb7ae3
Pekka Tuukkanen <petuukka@gmail.com>
2024-01-14 12:08:59 +0100

    Ex09 done

--- src/ex09.rs
+++ src/ex09.rs
@@ -1,10 +1,6 @@
 use std::collections::BTreeMap;
 
-use crate::{
-    proposition::Proposition,
-    propositional_formula::PropositionalFormula,
-    set::{complement, intersection, union},
-};
+use crate::{proposition::Proposition, propositional_formula::PropositionalFormula, set::*};
 
 impl Proposition {
     pub fn evaluate_set(&self, variable_map: &BTreeMap<char, Vec<i32>>) -> Vec<i32> {
@@ -15,9 +11,19 @@ impl Proposition {
             Proposition::Disjunction(p, q) => {
                 union(&p.evaluate_set(variable_map), &q.evaluate_set(variable_map))
             }
-            Proposition::ExclusiveDisjunction(p, q) => todo!(),
-            Proposition::LogicalEquivalence(p, q) => todo!(),
-            Proposition::MaterialCondition(p, q) => todo!(),
+            Proposition::LogicalEquivalence(p, q) => logical_equivalence(
+                &p.evaluate_set(variable_map),
+                &q.evaluate_set(variable_map),
+                variable_map,
+            ),
+            Proposition::ExclusiveDisjunction(p, q) => {
+                symmetric_difference(&p.evaluate_set(variable_map), &q.evaluate_set(variable_map))
+            }
+            Proposition::MaterialCondition(p, q) => material_conditional(
+                &p.evaluate_set(variable_map),
+                &q.evaluate_set(variable_map),
+                variable_map,
+            ),
             Proposition::Negation(p) => complement(&p.evaluate_set(variable_map), variable_map),
             Proposition::Variable(x) => variable_map.get(x).unwrap().to_vec(),
         }
@@ -25,23 +31,24 @@ impl Proposition {
 }
 
 pub fn eval_set(formula: &str, sets: Vec<Vec<i32>>) -> Vec<i32> {
-    if let Ok(pf) = PropositionalFormula::try_from(formula) {
-        assert_eq!(
-            sets.len(),
-            pf.variables.len(),
-            "Amount of sets must equal to the amount of variables in formula."
-        );
+    match PropositionalFormula::try_from(formula) {
+        Ok(pf) => {
+            if sets.len() != pf.variables.len() {
+                println!("Error");
+                return vec![];
+            }
 
-        let mut variables = pf.variables.clone().into_iter().collect::<Vec<char>>();
-        variables.sort();
-        let variable_map: BTreeMap<char, Vec<i32>> = variables.into_iter().zip(sets).collect();
-        dbg!(&variable_map);
-        let mut result = pf.evaluate_set(&variable_map);
-        result.sort();
-        result
-    } else {
-        println!("Error");
-        vec![]
+            let mut variables = pf.variables.clone().into_iter().collect::<Vec<char>>();
+            variables.sort();
+            let variable_map: BTreeMap<char, Vec<i32>> = variables.into_iter().zip(sets).collect();
+            let mut result = pf.evaluate_set(&variable_map);
+            result.sort();
+            result
+        }
+        Err(_) => {
+            println!("Error");
+            vec![]
+        }
     }
 }
 
@@ -64,5 +71,35 @@ fn test_eval_set() {
         eval_set("A!B|", vec![vec![0, 1, 2], vec![3, 4, 5]]),
         vec![3, 4, 5]
     );
+
     assert_eq!(eval_set("A!", vec![vec![0, 1, 2]]), vec![]);
+
+    assert_eq!(eval_set("A", vec![vec![]]), vec![]);
+    assert_eq!(eval_set("A!", vec![vec![]]), vec![]);
+    assert_eq!(eval_set("A", vec![vec![42]]), vec![42]);
+    assert_eq!(eval_set("A!", vec![vec![42]]), vec![]);
+
+    assert_eq!(
+        eval_set("A!B&", vec![vec![1, 2, 3], vec![2, 3, 4]]),
+        vec![4]
+    );
+    assert_eq!(eval_set("AB|", vec![vec![0, 1, 2], vec![]]), vec![0, 1, 2]);
+    assert_eq!(eval_set("AB&", vec![vec![0, 1, 2], vec![]]), vec![]);
+    assert_eq!(eval_set("AB&", vec![vec![0, 1, 2], vec![0]]), vec![0]);
+    assert_eq!(eval_set("AB&", vec![vec![0, 1, 2], vec![42]]), vec![]);
+    assert_eq!(eval_set("AB^", vec![vec![0, 1, 2], vec![0]]), vec![1, 2]);
+    assert_eq!(eval_set("AB>", vec![vec![0], vec![1, 2]]), vec![1, 2]);
+    assert_eq!(eval_set("AB>", vec![vec![0], vec![0, 1, 2]]), vec![0, 1, 2]);
+
+    assert_eq!(eval_set("ABC||", vec![vec![], vec![], vec![]]), vec![]);
+    assert_eq!(
+        eval_set("ABC||", vec![vec![0], vec![1], vec![2]]),
+        vec![0, 1, 2]
+    );
+    assert_eq!(eval_set("ABC||", vec![vec![0], vec![0], vec![0]]), vec![0]);
+    assert_eq!(eval_set("ABC&&", vec![vec![0], vec![0], vec![]]), vec![]);
+    assert_eq!(eval_set("ABC&&", vec![vec![0], vec![0], vec![0]]), vec![0]);
+    assert_eq!(eval_set("ABC^^", vec![vec![0], vec![0], vec![0]]), vec![0]);
+    assert_eq!(eval_set("ABC>>", vec![vec![0], vec![0], vec![0]]), vec![0]);
+    assert_eq!(eval_set("AB=C&", vec![vec![0,1,2], vec![1,2,3], vec![0,1,2,3,4,5]]), vec![1,2,4,5]);
 }
